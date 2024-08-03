b1ad0ef2eeb4be2b5a64b0e2faa53d4cbabbd8c2
Smit Bhamwala <smitbhamwala@gmail.com>
2024-08-03 23:08:18 +0530

    Updated the Projects section

--- components/project.tsx
+++ components/project.tsx
@@ -53,7 +53,7 @@ export default function Project({
 				</div>
 
 				<Image
-					src={imageUrl}
+					src={imageUrl[0]}
 					alt="Project I worked on"
 					// quality={95}
 					className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
