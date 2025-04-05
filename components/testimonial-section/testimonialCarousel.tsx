cae38f93bd3eab8c51600e6dd923c9b853ef6147
Smit Bhamwala <smitbhamwala@gmail.com>
2025-04-06 01:29:40 +0530

    Add testimonial pagination dots

--- components/testimonial-section/testimonialCarousel.tsx
+++ components/testimonial-section/testimonialCarousel.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import { Session } from "next-auth";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "../ui/carousel";
-import MyReviewCard from "./myReviewCard";
-import TestimonialCard from "./testimonialCard";
+  type CarouselApi
+} from "@/components/ui/carousel";
 import { TestimonialType } from "@/lib/types";
 import Autoplay from "embla-carousel-autoplay";
+import { Session } from "next-auth";
+import { useEffect, useState } from "react";
 import { Skeleton } from "../ui/skeleton";
+import MyReviewCard from "./myReviewCard";
+import TestimonialCard from "./testimonialCard";
 
 export interface TestimonialCarouselProps {
   session: Session | null;
@@ -31,57 +33,107 @@ export default function TestimonialCarousel({
   carouselClassName,
   carouselContentClassName,
   carouselItemClassName,
-  isSkeletonLoading,
+  isSkeletonLoading
 }: TestimonialCarouselProps) {
-  return (
-    <Carousel
-      opts={{
-        align: "start",
-        // loop: true
-      }}
-      plugins={[
-        Autoplay({
-          delay: 4000,
-        }),
-      ]}
-      orientation={orientation}
-      className={carouselClassName}
-    >
-      <CarouselContent className={carouselContentClassName}>
-        {session && !isSkeletonLoading && (
-          <CarouselItem className={carouselItemClassName}>
-            <MyReviewCard />
-          </CarouselItem>
-        )}
-        {isSkeletonLoading && (
-          <>
-            <CarouselItem className={carouselItemClassName}>
-              <Skeleton className="borderBlack rounded-xl shadow-none h-[17rem] bg-[#f3f4f6] dark:bg-gray-800" />
-            </CarouselItem>
-            <CarouselItem className={carouselItemClassName}>
-              <Skeleton className="borderBlack rounded-xl shadow-none h-[17rem] bg-[#f3f4f6] dark:bg-gray-800" />
-            </CarouselItem>
-            <CarouselItem className={carouselItemClassName}>
-              <Skeleton className="borderBlack rounded-xl shadow-none h-[17rem] bg-[#f3f4f6] dark:bg-gray-800" />
-            </CarouselItem>
-          </>
-        )}
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
 
-        {testimonials.map((testimonial: TestimonialType) => (
-          <CarouselItem key={testimonial._id} className={carouselItemClassName}>
-            <TestimonialCard
-              name={testimonial.name}
-              email={testimonial.email}
-              image={testimonial.image}
-              rating={testimonial.rating}
-              review={testimonial.review}
-              LinkedInId={testimonial.LinkedInId}
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+  useEffect(() => {
+    if (!api) return;
+
+    const updateCurrent = () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    };
+
+    const updateCount = () => {
+      const snapList = api.scrollSnapList();
+      setCount(snapList.length);
+    };
+
+    updateCount();
+    updateCurrent();
+
+    api.on("select", updateCurrent);
+    api.on("reInit", updateCount);
+    api.on("reInit", updateCurrent);
+
+    return () => {
+      api.off("select", updateCurrent);
+      api.off("reInit", updateCount);
+      api.off("reInit", updateCurrent);
+    };
+  }, [api]);
+
+  return (
+    <div>
+      <Carousel
+        setApi={setApi}
+        opts={{
+          align: "start"
+          // loop: true
+        }}
+        plugins={[
+          Autoplay({
+            delay: 4000
+          })
+        ]}
+        orientation={orientation}
+        className={carouselClassName}>
+        <CarouselContent className={carouselContentClassName}>
+          {isSkeletonLoading ? (
+            <>
+              <CarouselItem className={carouselItemClassName}>
+                <Skeleton className="borderBlack rounded-xl shadow-none h-[17rem] bg-[#f3f4f6] dark:bg-gray-800" />
+              </CarouselItem>
+              <CarouselItem className={carouselItemClassName}>
+                <Skeleton className="borderBlack rounded-xl shadow-none h-[17rem] bg-[#f3f4f6] dark:bg-gray-800" />
+              </CarouselItem>
+              <CarouselItem className={carouselItemClassName}>
+                <Skeleton className="borderBlack rounded-xl shadow-none h-[17rem] bg-[#f3f4f6] dark:bg-gray-800" />
+              </CarouselItem>
+            </>
+          ) : (
+            <>
+              {session && (
+                <CarouselItem className={carouselItemClassName}>
+                  <MyReviewCard />
+                </CarouselItem>
+              )}
+              {testimonials.map((testimonial: TestimonialType) => (
+                <CarouselItem
+                  key={testimonial._id}
+                  className={carouselItemClassName}>
+                  <TestimonialCard
+                    name={testimonial.name}
+                    email={testimonial.email}
+                    image={testimonial.image}
+                    rating={testimonial.rating}
+                    review={testimonial.review}
+                    LinkedInId={testimonial.LinkedInId}
+                  />
+                </CarouselItem>
+              ))}
+            </>
+          )}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="hidden md:block">
+        {count > 1 && (
+          <div className="flex justify-center gap-2 mt-4">
+            {Array.from({ length: count }).map((_, i) => (
+              <div
+                key={i}
+                onClick={() => api?.scrollTo(i)}
+                className={`h-2 w-2 rounded-full cursor-pointer transition-all duration-300 bg-gray-300 dark:bg-gray-700 
+                ${i + 1 === current ? "bg-gray-700 dark:bg-gray-300 scale-110" : ""}`}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
