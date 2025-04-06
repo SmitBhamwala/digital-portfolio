2c2830d8afae27bc5c14db98468197446d2d155a
Smit Bhamwala <smitbhamwala@gmail.com>
2025-04-06 22:31:24 +0530

    Reduce navbar link's gap on laptop

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -13,12 +13,12 @@ export default function Header() {
 	return (
 		<header className="z-[999] relative">
 			<motion.div
-				className="fixed top-0 left-1/2 p-0 h-[3.85rem] w-full rounded-none border border-white border-opacity-40 bg-slate-100 bg-opacity-80 shadow-lg shadow-black/[0.15] backdrop-blur-[0.5rem] md:top-6 md:h-[3.25rem] md:w-[53.5rem] md:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+				className="fixed top-0 left-1/2 p-0 h-[3.85rem] w-full rounded-none border border-white border-opacity-40 bg-slate-100 bg-opacity-80 shadow-lg shadow-black/[0.15] backdrop-blur-[0.5rem] md:top-6 md:h-[3.25rem] md:w-[48.5rem] md:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
 				initial={{ y: -100, x: "-50%", opacity: 0 }}
 				animate={{ y: 0, x: "-50%", opacity: 1 }}></motion.div>
 
 			<nav className="flex fixed top-0 left-1/2 h-auto md:h-[2.875rem] -translate-x-1/2 py-2 md:top-[1.7rem] md:py-0">
-				<ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+				<ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5 md:gap-2">
 					{links.map((link) => (
 						<motion.li
 							className="h-3/4 flex items-center justify-center relative"
