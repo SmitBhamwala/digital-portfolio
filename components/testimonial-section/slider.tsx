b8cd80511254ccb3dfd0b917b81f684116c98d8c
Smit Bhamwala <smitbhamwala@gmail.com>
2025-04-06 12:35:44 +0530

    Fix testimonialsWithoutMyReview filter

--- components/testimonial-section/slider.tsx
+++ components/testimonial-section/slider.tsx
@@ -6,98 +6,103 @@ import { useEffect, useState } from "react";
 import TestimonialCarousel from "./testimonialCarousel";
 
 function shuffleTestimonials(array: any) {
-	const shuffled = [...array]; // Clone the array to avoid mutation
-	for (let i = shuffled.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-	}
-	return shuffled;
+  const shuffled = [...array]; // Clone the array to avoid mutation
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
 }
 
 export default function Slider() {
-	const [testimonials, setTestimonials] = useState([]);
-	const [loadingTestimonials, setLoadingTestimonials] = useState(true);
-	const { data: session } = useSession();
+  const [testimonials, setTestimonials] = useState([]);
+  const [loadingTestimonials, setLoadingTestimonials] = useState(true);
+  const { data: session } = useSession();
+  const [count, setCount] = useState(0);
 
-	useEffect(() => {
-		async function fetchPosts() {
-			const res = await fetch("/api/testimonial", {
-				cache: "default",
-				headers: {
-					"x-secret-key": process.env.NEXT_PUBLIC_API_SECRET || ""
-				}
-			});
-			const data = await res.json();
-			const testimonialData = data.filter(
-				(testimonial: TestimonialType) => testimonial.review !== ""
-			);
-			const randomTestimonials: any = shuffleTestimonials(testimonialData);
-			if (session) {
-				const testimonialsWithoutMyReview = randomTestimonials.filter(
-					(testimonial: TestimonialType) =>
-						testimonial.email !== session.user?.email
-				);
-				setTestimonials(testimonialsWithoutMyReview);
-			} else {
-				setTestimonials(randomTestimonials);
-			}
-		}
-		setLoadingTestimonials(true);
-		fetchPosts();
-		setLoadingTestimonials(false);
-	}, []);
+  useEffect(() => {
+    if (count) {
+      return;
+    }
+    async function fetchPosts() {
+      const res = await fetch("/api/testimonial", {
+        cache: "default",
+        headers: {
+          "x-secret-key": process.env.NEXT_PUBLIC_API_SECRET || ""
+        }
+      });
+      const data = await res.json();
+      const testimonialData = data.filter(
+        (testimonial: TestimonialType) => testimonial.review !== ""
+      );
+      const randomTestimonials: any = shuffleTestimonials(testimonialData);
+      if (session) {
+        const testimonialsWithoutMyReview = randomTestimonials.filter(
+          (testimonial: TestimonialType) =>
+            testimonial.email !== session.user?.email
+        );
+        setTestimonials(testimonialsWithoutMyReview);
+        setCount(count + 1);
+      } else {
+        setTestimonials(randomTestimonials);
+      }
+    }
+    setLoadingTestimonials(true);
+    fetchPosts();
+    setLoadingTestimonials(false);
+  }, [session]);
 
-	return (
-		<div className="transition-all md:h-[16.7rem]">
-			{loadingTestimonials ? (
-				<div>
-					{/* For Desktop Loader */}
-					<TestimonialCarousel
-						orientation="horizontal"
-						carouselClassName="w-[65vw] hidden md:block select-none"
-						carouselContentClassName="p-2"
-						carouselItemClassName="md:basis-1/2 lg:basis-1/3"
-						session={session}
-						testimonials={testimonials}
-						isSkeletonLoading={loadingTestimonials}
-					/>
+  return (
+    <div className="transition-all md:h-[16.7rem]">
+      {loadingTestimonials ? (
+        <div>
+          {/* For Desktop Loader */}
+          <TestimonialCarousel
+            orientation="horizontal"
+            carouselClassName="w-[65vw] hidden md:block select-none"
+            carouselContentClassName="p-2"
+            carouselItemClassName="md:basis-1/2 lg:basis-1/3"
+            session={session}
+            testimonials={testimonials}
+            isSkeletonLoading={loadingTestimonials}
+          />
 
-					{/* For Mobile Loader */}
-					<TestimonialCarousel
-						orientation="vertical"
-						carouselClassName="block md:hidden select-none mt-16"
-						carouselContentClassName="h-[34rem]"
-						carouselItemClassName="basis-1/2 hover:cursor-grab active:cursor-grabbing"
-						session={session}
-						testimonials={testimonials}
-						isSkeletonLoading={loadingTestimonials}
-					/>
-				</div>
-			) : (
-				<div>
-					{/* For Desktop */}
-					<TestimonialCarousel
-						orientation="horizontal"
-						carouselClassName="hidden md:block select-none"
-						carouselContentClassName="p-2"
-						carouselItemClassName="md:basis-1/2 lg:basis-1/3 hover:cursor-grab active:cursor-grabbing"
-						session={session}
-						testimonials={testimonials}
-						isSkeletonLoading={loadingTestimonials}
-					/>
+          {/* For Mobile Loader */}
+          <TestimonialCarousel
+            orientation="vertical"
+            carouselClassName="block md:hidden select-none mt-16"
+            carouselContentClassName="h-[34rem]"
+            carouselItemClassName="basis-1/2 hover:cursor-grab active:cursor-grabbing"
+            session={session}
+            testimonials={testimonials}
+            isSkeletonLoading={loadingTestimonials}
+          />
+        </div>
+      ) : (
+        <div>
+          {/* For Desktop */}
+          <TestimonialCarousel
+            orientation="horizontal"
+            carouselClassName="hidden md:block select-none"
+            carouselContentClassName="p-2"
+            carouselItemClassName="md:basis-1/2 lg:basis-1/3 hover:cursor-grab active:cursor-grabbing"
+            session={session}
+            testimonials={testimonials}
+            isSkeletonLoading={loadingTestimonials}
+          />
 
-					{/* For Mobile */}
-					<TestimonialCarousel
-						orientation="vertical"
-						carouselClassName="block md:hidden select-none mt-16"
-						carouselContentClassName="h-[34rem]"
-						carouselItemClassName="basis-1/2 hover:cursor-grab active:cursor-grabbing"
-						session={session}
-						testimonials={testimonials}
-						isSkeletonLoading={loadingTestimonials}
-					/>
-				</div>
-			)}
-		</div>
-	);
+          {/* For Mobile */}
+          <TestimonialCarousel
+            orientation="vertical"
+            carouselClassName="block md:hidden select-none mt-16"
+            carouselContentClassName="h-[34rem]"
+            carouselItemClassName="basis-1/2 hover:cursor-grab active:cursor-grabbing"
+            session={session}
+            testimonials={testimonials}
+            isSkeletonLoading={loadingTestimonials}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
