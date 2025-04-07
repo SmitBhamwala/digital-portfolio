3a7553162c908b9a83c6ea1e1492059c9da12b0d
Smit Bhamwala <smitbhamwala@gmail.com>
2025-04-08 02:52:19 +0530

    Suspense consistent html

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -5,7 +5,12 @@ import SignInRedirect from "./SignInRedirect";
 
 export default function SignInPage() {
   return (
-    <Suspense fallback={<p>Redirecting...</p>}>
+    <Suspense
+      fallback={
+        <p className="h-[100vh] w-[100vw] flex items-center justify-center">
+          Redirecting back...
+        </p>
+      }>
       <SignInRedirect />
     </Suspense>
   );
