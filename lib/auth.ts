fdd7e1370073394d18f3331fe80b0745145659cb
Smit Bhamwala <smitbhamwala@gmail.com>
2025-04-08 00:59:53 +0530

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,11 +5,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
   providers: [
     LinkedIn({
+      clientId: process.env.AUTH_LINKEDIN_ID || "",
+      clientSecret: process.env.AUTH_LINKEDIN_SECRET || "",
       authorization: {
         params: {
           scope: "openid profile email"
         }
       }
     })
-  ]
+  ],
+  secret: process.env.AUTH_SECRET
 });
