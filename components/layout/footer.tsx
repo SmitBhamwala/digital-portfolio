95f1a40ebf7f5ce2c4e0a49506e191ebea51033c
Smit Bhamwala <smitbhamwala@gmail.com>
2025-04-10 01:14:33 +0530

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="mb-5 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; {currentYear} — Designed & developed by{" "}
+        &copy; {currentYear} — Developed by{" "}
         <Link
           href="https://www.linkedin.com/in/smitbhamwala"
           target="_blank"
