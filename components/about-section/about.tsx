508a1ca0d460773b505899a5eed6f9b2844c4c34
Smit Bhamwala <smitbhamwala@gmail.com>
2025-07-02 00:45:09 +0530

    Update about.tsx

--- components/about-section/about.tsx
+++ components/about-section/about.tsx
@@ -117,12 +117,12 @@ export default function About() {
               e.preventDefault();
               showToast("success", "CV downloaded successfully!");
               // Trigger the download after showing the toast
-              // const link = document.createElement("a");
-              // link.href = "/Smit Bhamwala CV.pdf";
-              // link.download = "Smit Bhamwala CV.pdf";
-              // document.body.appendChild(link);
-              // link.click();
-              // document.body.removeChild(link);
+              const link = document.createElement("a");
+              link.href = "/Smit Bhamwala CV.pdf";
+              link.download = "Smit Bhamwala CV.pdf";
+              document.body.appendChild(link);
+              link.click();
+              document.body.removeChild(link);
             }}
             className="lg:w-fit text-center justify-center bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-2xl outline-hidden hover:bg-gray-950 active:scale-95 dark:bg-gray-500 transition">
             Download CV{" "}
